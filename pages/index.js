93361a3b014bab08c04d7234b6f435f8829cadd9
bebenx <bebenx@gmail.com>
2023-10-12 10:59:11 +0700

    Tugas9

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,22 @@
 // import Footer from "@/components/footer";
 
 import Layout from "@/layout";
+import { useEffect } from "react";
 
 export default function Main({ children, metaTitle, metaDescription }) {
- return (
-  <div>
-   <Layout metaTitle="Home">
-    <p>Home</p>
-   </Layout>
-  </div>
- );
+
+  useEffect(()=>{
+    fetch("/api/hello")
+    .then((res) => res.json())
+    .then((res)=> console.log("response ", res))
+    .catch((err)=> console.log("err => ", err))
+  }, [])
+
+  return (
+    <div>
+    <Layout metaTitle="Home">
+      <p>Home</p>
+    </Layout>
+    </div>
+  );
 }
