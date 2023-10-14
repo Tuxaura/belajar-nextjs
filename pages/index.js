07e4af70af415b63c28f5dccbd544e7ca2c10f0e
bebenx <bebenx@gmail.com>
2023-10-14 08:00:27 +0700

    Tugas10

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,35 @@
 // import Header from "@/components/header";
 // import Footer from "@/components/footer";
 
-import Layout from "@/layout";
-import { useEffect } from "react";
+// import Layout from "@/layout";
+// import Image from "next/image";
+// import { useEffect } from "react";
+import dynamic from "next/dynamic";
+
+const LayoutComponent = dynamic(()=>import("@/layout"))
 
 export default function Main({ children, metaTitle, metaDescription }) {
 
-  useEffect(()=>{
-    fetch("/api/hello")
-    .then((res) => res.json())
-    .then((res)=> console.log("response ", res))
-    .catch((err)=> console.log("err => ", err))
-  }, [])
+  // useEffect(()=>{
+  //   fetch("/api/hello")
+  //   .then((res) => res.json())
+  //   .then((res)=> console.log("response ", res))
+  //   .catch((err)=> console.log("err => ", err))
+  // }, [])
 
   return (
     <div>
-    <Layout metaTitle="Home">
+      <LayoutComponent metaTitle={"Home"}>
+     {/* <Layout metaTitle="Home"> */}
       <p>Home</p>
-    </Layout>
+      {/* <Image src="/haircut.png" width={500} height={400} alt="barber img"/>
+      <img 
+        src="/haircut.png"
+        style={{ width:500, height: 400 }}
+        alt="barber img"
+      /> */}
+      {/* </Layout> */}
+      </LayoutComponent>
     </div>
   );
 }
